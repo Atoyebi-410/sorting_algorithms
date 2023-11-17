61ef1916d6e45df7cef72fcac7520ac45a9c1c27
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-11-17 04:17:52 -0800

    sorting algorithms

--- sort.h
+++ sort.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 
 
-/** Struct Double Linked List */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -39,8 +38,7 @@ void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void merge_recursion(int *arr, int *array, size_t left, size_t right);
-void merge_subarray(int *arr, int *array, size_t left,
-		size_t middle, size_t right);
+void merge_subarray(int *arr, int *array, size_t left, size_t middle, size_t right);
 void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
